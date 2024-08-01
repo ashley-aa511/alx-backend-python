ffba6adad7926269d295ee445519f804aa695b3d
ashley-aa511 <okeyoashley@gmail.com>
2024-08-01 05:45:13 +0000

    type annonated functions

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+""" Define the functions with type anonations"""
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2;
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Define a type function that takes a float and returns a string repof the float"""
+def to_str(n: float) -> str:
+    return str(n)
